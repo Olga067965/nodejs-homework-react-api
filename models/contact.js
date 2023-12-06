a4d2800fe776e0eee2b105a04942019c3d94fc14
Olga067965 <olgasavelieva5@gmail.com>
2023-12-06 19:34:11 +0100

    Update2

---
+++ models/contact.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const { addSchema, changeFavoriteSchema } = require("../schemas/schemas");
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+contactSchema.post("save", handleMongooseError);
+
+const Contact = model("contact", contactSchema);
+const schemas = { addSchema, changeFavoriteSchema };
+
+module.exports = { Contact, schemas };
